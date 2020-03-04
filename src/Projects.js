fcdd8e930338af5e4d5fec9cc7ef0f2a8c4a30e7
Colin But <colin.but@outlook.com>
2020-03-04 14:19:13 +0000

    Grid layout

--- src/Projects.js
+++ src/Projects.js
@@ -1,27 +1,116 @@
 import React from 'react'
 import './index.css';
 import { Link, useRouteMatch } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import CeramicsImg from './img/ceramics.jpg'
+import WatchesImg from './img/watches.jpg'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
 
+const useStyles = makeStyles({
+    media: {
+        // height: 354.38
+        minHeight: 354.38
+    }
+})
 
-export default function Projects() {
-
+const Projects = () => {
+    const classes = useStyles();
     let { url } = useRouteMatch();
-    console.log(url);
+    console.log(CeramicsImg);
 
     return (
         <div>
-            <h1>Projects</h1>
-            <ul>
-                <li>
-                    <Link to={`${url}/project1`}>Project 1</Link>
-                </li>
-                <li>
-                    <Link to={`${url}/project2`}>Project 2</Link>
-                </li>
-                <li>
-                    <Link to={`${url}/project3`}>Project 3</Link>
-                </li>
-            </ul>
+            <Grid container spacing={8}>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project1`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={CeramicsImg} title="Ceramics"/>
+                                <CardContent>
+                                    HKPC Academy
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project2`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={WatchesImg} title="Watches"/>
+                                <CardContent>
+                                    5 Past Watches
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project1`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={CeramicsImg} title="Ceramics"/>
+                                <CardContent>
+                                    HKPC Academy
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project2`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={WatchesImg} title="Watches"/>
+                                <CardContent>
+                                    5 Past Watches
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project1`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={CeramicsImg} title="Ceramics"/>
+                                <CardContent>
+                                    HKPC Academy
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to={`${url}/project2`}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia className={classes.media} image={WatchesImg} title="Watches"/>
+                                <CardContent>
+                                    5 Past Watches
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+            </Grid>
+            <Description />
         </div>
     )
 }
+
+function Description() {
+    return (
+        <div>
+            <p>
+                Hollie Beaumont is a photographer and art director based in London. She shoots a diverse range of subjects for commercial and editorial clients, all with a clean and elegant aesthetic.
+            </p>
+        </div>
+    )
+}
+
+export default Projects
