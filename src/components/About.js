7f8f2b87429591764ac87696f556d0e2b7ba2d57
Colin But <colin.but@outlook.com>
2020-03-05 12:20:13 +0000

    stop hardcoding text in js

--- src/components/About.js
+++ src/components/About.js
@@ -2,33 +2,23 @@ import React from 'react'
 import '../css/index.css';
 import Footer from './Footer'
 import Grid from '@material-ui/core/Grid'
+import data from '../config/about.json'
 
 export default function About() {
     return (
         <div className="about-page-container-wrapper">
-            <h1>UI & UX Designer</h1>
-            <p><em>based in London and Hong Kong</em></p>
-            <p>
-                I am enthusiastic and passionate about Design Thinking Process. I love
-                working with different clients because there are so many unknown challenges 
-                which keeps me motivated and stay curious. I enjoy creating meaningful and 
-                delightful digital products through the use of design.
-            </p>
+            <h1></h1>
+            <h1>{data.heading}</h1>
+            <p><em>{data.subHeading}</em></p>
+            <p>{data.summary}</p>
             <Grid container spacing={10}>
                 <Grid item xs={6}>
                     <h2>Services</h2>
-                    <p>
-                        Web Design / Mobile Design / Interactive Prototyping
-                        / User Interface / UX Design / Visual Design / Design Research /
-                        Interactive Prototyping / Video Editing
-                    </p>
+                    <p>{data.services}</p>
                 </Grid>
                 <Grid item xs={6}>
                     <h2>Clients</h2>
-                    <p>
-                        HKPC Academy / Being Sent Travel / TBM The Beauty Medical /
-                        Beng Seng / Harbour City / apm
-                    </p>
+                    <p>{data.clients}</p>
                 </Grid>
             </Grid>
             <Footer />
