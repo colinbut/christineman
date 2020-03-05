4dee9c9c1c7be85a1258c220af0ee52aaa41fdaa
Colin But <colin.but@outlook.com>
2020-03-05 23:14:48 +0000

    merging config jsons

--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,23 @@ import React from 'react'
 import '../css/index.css';
 import Footer from './Footer'
 import Grid from '@material-ui/core/Grid'
-import data from '../config/about.json'
+import data from '../config/static.json'
 
 export default function About() {
+    const about = data.about
     return (
         <div className="about-page-container-wrapper">
-            <h1>{data.heading}</h1>
-            <p><em>{data.subHeading}</em></p>
-            <p>{data.summary}</p>
+            <h1>{about.heading}</h1>
+            <p><em>{about.subHeading}</em></p>
+            <p>{about.summary}</p>
             <Grid container spacing={5}>
                 <Grid item xs={6}>
                     <h2>Services</h2>
-                    <p>{data.services}</p>
+                    <p>{about.services}</p>
                 </Grid>
                 <Grid item xs={6}>
                     <h2>Clients</h2>
-                    <p>{data.clients}</p>
+                    <p>{about.clients}</p>
                 </Grid>
             </Grid>
             <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../css/index.css';
+import data from '../config/static.json'
 
 export default function Footer() {
+    const copyright = data.copyright
     return (
         <div className="footer">
             <strong>Copyright</strong>
-            <p>&copy; 2020 Christine Man</p>
+            <p>{copyright.text}</p>
         </div>
     )
 }
