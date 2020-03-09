b6ac7b86f3b4623301732b3f3c2462979cbd9350
Colin But <colin.but@outlook.com>
2020-03-09 18:43:59 +0000

    moved images to public img folder and refactor to use static.json to read in data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,6 @@ import React from 'react'
 import '../css/index.css';
 import { Link, useRouteMatch } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
-import CeramicsImg from '../img/ceramics.jpg'
-import WatchesImg from '../img/watches.jpg'
-import HKPC from '../img/HKPC.jpeg'
-import HKPC_Portfolio from '../img/HKPC_Portfolio.jpeg'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
@@ -13,6 +9,7 @@ import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Footer from '../components/Footer'
 import { NavLink } from 'react-router-dom'
+import data from '../config/static.json'
 
 const useStyles = makeStyles({
     root: {
@@ -25,55 +22,16 @@ const useStyles = makeStyles({
     }
 })
 
-const projectData = [
-    {
-        id: 1,
-        name: "HKPC Academy",
-        image: HKPC,
-        title: "Ceramics"
-    },
-    {
-        id: 2,
-        name: "5 Past Watches",
-        image: WatchesImg,
-        title: "Watches"
-    },
-    {
-        id: 3,
-        name: "HKPC Academy",
-        image: HKPC,
-        title: "Ceramics"
-    },
-    {
-        id: 4,
-        name: "5 Past Watches",
-        image: WatchesImg,
-        title: "Watches"
-    },
-    {
-        id: 5,
-        name: "HKPC Academy",
-        image: HKPC,
-        title: "Ceramics"
-    },
-    {
-        id: 6,
-        name: "5 Past Watches",
-        image: WatchesImg,
-        title: "Watches"
-    }
-]
-
 const Projects = () => {
     const classes = useStyles();
     let { url } = useRouteMatch();
-
+    const projectData = data.projects.projectData
     const projectList = projectData.map((item, key) => 
         <Grid item xs={6}>
             <Link to={`${url}/${item.id}`}>
                 <Card square={true}>
                     <CardActionArea>
-                        <CardMedia className={classes.media} image={item.image} title={item.title}/>
+                        <CardMedia className={classes.media} image={process.env.PUBLIC_URL + '/img/' + item.image} title={item.title}/>
                         <CardContent className={classes.root}>
                             { item.name }
                         </CardContent>
@@ -98,9 +56,7 @@ const Description = () => {
     return (
         <div>
             <h3 className="description">
-                Hollie Beaumont is a photographer and art director based in London. 
-                She shoots a diverse range of subjects for commercial and editorial clients, all with a clean and elegant 
-                aesthetic.
+                {data.projects.description}
             </h3>
             <div className="link">
             <NavLink to="/contact">
--- src/components/ui/ArrowButton.js
+++ src/components/ui/ArrowButton.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import '../../css/index.css'
 
 const ArrowButton = (props) => {
     const { buttonText, ArrowButton } = props
     return (
-        <div>
-            <p>{ buttonText }</p>
-            { ArrowButton }        
+        <div className="arrow-button">
+            <Grid container spacing={1}>
+                <Grid item xs={6}>
+                    <p className="arrow-button-text">{ buttonText }</p>
+                </Grid>
+                <Grid item xs={6}>
+                    { ArrowButton }        
+                </Grid>
+            </Grid>
         </div>
     )
 }
